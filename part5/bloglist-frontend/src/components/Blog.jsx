4a93a7483e06cee8eca0fc9b02e8a07a2c1a2dae
Andreea Csecs <andreeacsecs@yahoo.com>
2024-06-24 20:06:06 +0300

    Completed exercises 5.1 to 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,9 +1,37 @@
-//src/components/Blog.jsx
+import React, { useState } from "react";
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <strong>{blog.title}</strong> by {blog.author}{" "}
+        <button onClick={toggleDetails}>{showDetails ? "hide" : "view"}</button>
+      </div>
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
