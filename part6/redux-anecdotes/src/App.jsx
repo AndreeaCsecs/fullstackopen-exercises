158ebd233037af31acc6963b791256eb70c3d32c
Andreea Csecs <andreeacsecs@yahoo.com>
2024-08-05 15:32:06 +0300

    ex 6-6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,20 @@
 // src/App.jsx
 
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/AnecdoteFilter";
 import Notification from "./components/Notification";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initializeAnecdotes());
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -2,66 +2,62 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 import { showNotification } from "./notificationReducer";
-
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
+import anecdoteService from "../services/anecdoteService";
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState,
+  initialState: [],
   reducers: {
-    voteAnecdote(state, action) {
-      const id = action.payload;
-      const anecdoteToVote = state.find((anecdote) => anecdote.id === id);
-      if (anecdoteToVote) {
-        anecdoteToVote.votes += 1;
-      }
+    setAnecdotes(state, action) {
+      return action.payload;
     },
     addAnecdote(state, action) {
       state.push(action.payload);
     },
+    voteAnecdote(state, action) {
+      const id = action.payload;
+      const anecdoteToVote = state.find((anecdote) => anecdote.id === id);
+      if (anecdoteToVote) {
+        const updatedAnecdote = {
+          ...anecdoteToVote,
+          votes: anecdoteToVote.votes + 1,
+        };
+        return state.map((anecdote) =>
+          anecdote.id !== id ? anecdote : updatedAnecdote
+        );
+      }
+    },
   },
 });
 
-export const { voteAnecdote, addAnecdote } = anecdoteSlice.actions;
+export const { setAnecdotes, addAnecdote, voteAnecdote } =
+  anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
 
 export const createAnecdote = (content) => {
   return async (dispatch) => {
-    const newAnecdote = {
-      content,
-      id: getId(),
-      votes: 0,
-    };
+    const newAnecdote = await anecdoteService.createNew(content);
     dispatch(addAnecdote(newAnecdote));
-    dispatch(showNotification(`You added '${content}'`, 5));
+    dispatch(showNotification(`Added anecdote '${content}'`, 5));
   };
 };
 
 export const voteForAnecdote = (id) => {
   return async (dispatch, getState) => {
+    const anecdoteToVote = getState().anecdotes.find((a) => a.id === id);
+    const updatedAnecdote = {
+      ...anecdoteToVote,
+      votes: anecdoteToVote.votes + 1,
+    };
+    await anecdoteService.update(updatedAnecdote);
     dispatch(voteAnecdote(id));
-    const votedAnecdote = getState().anecdotes.find(
-      (anecdote) => anecdote.id === id
-    );
-    dispatch(showNotification(`You voted '${votedAnecdote.content}'`, 5));
+    dispatch(showNotification(`You voted for '${anecdoteToVote.content}'`, 5));
   };
 };
 
